1a1cad8575fccff26b35694f6a9bda5d46208af0
Benson Trinh <me@bensontrinh.com>
2012-10-28 15:32:38 -0600

    Created the basic structure for the UI. Will need to work on styling it afterwards. I still need to implement the skeleton code.
    Need to add stuff for button handlers, the TaskManager, JSONSever, etc.
    Will be working on fixing up the layout a bit then start on those.

---
+++ classproject/src/com/cmput301/classproject/MainActivity.java
@@ -0,0 +1,93 @@
+package com.cmput301.classproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    /**
+     * Name: 		handlePublicTasks
+     * Description: This will called when the Public Tasks button
+     * 				is clicked on the Main UI window
+     * 
+     * 				This should query the server and retrieve all
+     * 				public tasks and list them in the ListView
+     * 				of the Main UI window
+     * @param v
+     */
+    public void handlePublicTasks(View v) {
+    	
+    	//TODO - Implement handlePublicTasks
+    	
+    }
+    
+    /**
+     * Name: 		handleYourTasks
+     * Description: This will called when the Your Tasks button
+     * 				is clicked on the Main UI window
+     * 
+     * 				This should query the server and return and list
+     * 				the results on the ListView of the Main UI window
+     * 				of the owners specifically posted tasks
+     * @param v
+     */
+    public void handleYourTasks(View v) {
+    	//TODO - Implement handleYourTasks
+    }
+    
+    /**
+     * Name: 		handleSpecificTasks
+     * Description: This will called when the Specific button
+     * 				is clicked on the Main UI window
+     * 
+     * 				This will prompt the user using a dialog to
+     * 				enter the name of the user they wish to view
+     * 				tasks from. Upon inputting a VALID user, the
+     * 				list of users will be retrieved from the server
+     * 				and listed in the ListView of the Main UI activity.
+     * 
+     * 				If it is an INVALID user, we will display an error
+     * 				to the user and show an empty list to the user. 
+     * @param v
+     */
+    public void handleSpecificTasks(View v) {
+    	
+    }
+    
+    /**
+     * Name: 		onSyncHandler
+     * Description: The user will manually sync their content
+     * 				with the content on the server to allow
+     * 				for offline viewing
+     * @param v
+     */
+    public void onSyncHandler(View v) {
+    	
+    }
+    
+    
+    /**
+     * Name: 		addTaskHandler
+     * Description: This will redirect the user to the
+     *				Add Task activity
+     * @param v
+     */
+    public void addTaskHandler(View v) {
+    	
+    }
+    
+}
---
+++ classproject/src/com/cmput301/classproject/SubmissionViewActivity.java
@@ -0,0 +1,35 @@
+package com.cmput301.classproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class SubmissionViewActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission_view);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_submission_view, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
