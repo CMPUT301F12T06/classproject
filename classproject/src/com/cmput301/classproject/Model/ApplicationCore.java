f373295a5dda425a5196335f83f79071ee74c78c
Thomas Polasek <thomas.p@shaw.ca>
2012-10-31 21:36:53 -0600

    added license to each source file

--- classproject/src/com/cmput301/classproject/Model/ApplicationCore.java
+++ classproject/src/com/cmput301/classproject/Model/ApplicationCore.java
@@ -1,3 +1,20 @@
+/**
+License: GPL 2.0
+
+Copyright (C) 2012 Benson Trinh, Thomas Polasek, Remco Uittenbogerd, Clinton Wong
+
+This program is free software; you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software Foundation;
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+MA 02110-1301, USA.
+ **/
+
 package com.cmput301.classproject.Model;
 
 import android.app.Activity;
@@ -9,14 +26,11 @@ import android.widget.Toast;
 
 public class ApplicationCore extends Application {
 
-
 	public static void displayToastMessage(Context context, CharSequence text) {
 		int duration = Toast.LENGTH_SHORT;
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 	}
-
-
 
 	public static String getStringFromId(Activity act, int id) {
 		return (((TextView) act.findViewById(id)).getText().toString());
@@ -25,8 +39,6 @@ public class ApplicationCore extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-	
 
 		// pass our single Application instance to other singleton classes
 		JSONServer.getInstance().setApplicatonReference(this);
