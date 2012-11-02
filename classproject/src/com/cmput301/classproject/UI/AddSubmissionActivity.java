00d76d0e9799b432b0993e367766955dde7008f4
Clinton <clint.isaac.wong@gmail.com>
2012-11-02 12:19:27 -0600

    Implemented the photo capture for a submission.

--- classproject/src/com/cmput301/classproject/UI/AddSubmissionActivity.java
+++ classproject/src/com/cmput301/classproject/UI/AddSubmissionActivity.java
@@ -16,27 +16,43 @@ MA 02110-1301, USA.
  **/
 package com.cmput301.classproject.UI;
 
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
 import com.cmput301.classproject.R;
 import com.cmput301.classproject.Model.ApplicationCore;
 import com.cmput301.classproject.Model.Task;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.support.v4.app.NavUtils;
 
-public class AddSubmissionActivity extends Activity {
+public class AddSubmissionActivity extends Activity implements Observer {
 
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	private Uri fileUri;
 
 	private Task task = null; // reference to our task (this is passed by
 								// taskviewactivity)
 
+	ListView addedPhotos;
+	ListView addedAudio;
+	ArrayList<Bitmap> photosTaken = new ArrayList<Bitmap>();
+	
 	@Override 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -54,9 +70,23 @@ public class AddSubmissionActivity extends Activity {
 			ApplicationCore.displayToastMessage(this, "Obtained Task Id: "
 					+ task.getId());
 		}
-
+		
+		
 	}
 
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+		// Recognize the Views
+		addedPhotos = (ListView) findViewById(R.id.photos_added);
+		addedAudio = (ListView) findViewById(R.id.audio_added);
+		
+		addedPhotos.setAdapter(new ImageAdapter(this));
+	}
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_add_submission, menu);
@@ -115,21 +145,21 @@ public class AddSubmissionActivity extends Activity {
 	 * @param v
 	 */
 	public void addPhotoHandler(View v) {
-		// Intent cameraIntent = new
-		// Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		// startActivityForResult(cameraIntent,
-		// CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		 Intent cameraIntent = new
+		 Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		 startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
 
-	// protected void onActivityResult(int requestCode, int resultCode, Intent
-	// data) {
-	// if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode ==
-	// RESULT_OK)
-	// {
-	//
-	// }
-	//
-	// }
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode ==
+				RESULT_OK)
+		{
+			// Photo has been generated
+			Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+			photosTaken.add(thumbnail);
+		}
+
+	}
 
 	/**
 	 * Name: addAudioHandler Description: Similar to the photo handler but This
@@ -138,7 +168,8 @@ public class AddSubmissionActivity extends Activity {
 	 * @param v
 	 */
 	public void addAudioHandler(View v) {
-
+		// We're going to send a Toast that the button functionality is disabled right now.
+		ApplicationCore.displayToastMessage(getApplicationContext(), "Adding of Audio is not implemented right now.");
 	}
 
 	/**
@@ -164,6 +195,53 @@ public class AddSubmissionActivity extends Activity {
 		// method in TaskManager for an exmaple)
 		finish();
 
+	}
+
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * ImageAdapter Class to support putting thumbnails into grid views.
+	 * @author Crumbs
+	 *
+	 */
+	public class ImageAdapter extends BaseAdapter
+	{
+		private Context context;
+		
+		public ImageAdapter(Context C)
+		{
+			context = C;
+		}
+		
+		public int getCount() {
+			return photosTaken.size();
+		}
+
+		public Object getItem(int arg0) {
+			return photosTaken.get(arg0);
+		}
+
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new ListView.LayoutParams(185, 185));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(5, 5, 5, 5);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageBitmap(photosTaken.get(position));
+            return imageView;
+		}
+		
 	}
 
 }
