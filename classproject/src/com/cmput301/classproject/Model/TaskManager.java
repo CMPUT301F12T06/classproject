11684cc1a21ef8da9f459ea6361f1a12de3dc6fe
Benson Trinh <me@bensontrinh.com>
2012-11-03 22:39:13 -0600

    Fixing layout issue

--- classproject/src/com/cmput301/classproject/Model/TaskManager.java
+++ classproject/src/com/cmput301/classproject/Model/TaskManager.java
@@ -19,21 +19,37 @@ package com.cmput301.classproject.Model;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import android.app.Application;
-import com.cmput301.classproject.Model.JSONServer.Code;
+import android.content.Context;
+
+import com.cmput301.classproject.Model.Tasks.AddSubmission;
+import com.cmput301.classproject.Model.Tasks.JSONServer;
+import com.cmput301.classproject.Model.Tasks.JSONServer.Code;
+import com.cmput301.classproject.Model.Tasks.ModifyServerData;
+import com.cmput301.classproject.Model.Tasks.ReceiveServerData;
+import com.cmput301.classproject.Model.Tasks.SubmissionData;
 
 //Singleton object - We extend application so we can use the
 //ApplicationContext and treat this like a singleton.
 // Each additional singleton used in this project has a reference
 // to this application object so we can do operations like local file
-// storage amoung other singletons.
+// storage among other singletons.
 public class TaskManager extends Observable {
+	
+	private final static Logger LOGGER = Logger.getLogger(TaskManager.class.getName());
 
 	@SuppressWarnings("unused")
 	private Application appRef = null;
 	private static TaskManager instance = null;
 	private ArrayList<Observer> observers = null;
-
+	
+	public static ArrayList<Task> cachedTasks = new ArrayList<Task>();
+	public static String currentTask = null;
+	
 	private TaskManager() {
 		observers = new ArrayList<Observer>();
 	}
@@ -52,25 +68,65 @@ public class TaskManager extends Observable {
 		}
 	}
 
-	public Code addTask(Task task) {
+	public Code addTask(Task task, Context mContext) {
 
 		// TODO add creator field to task created using the phone serial
 
 		// TODO add connection logic and locale file storage stuff logic
-		Code returnCode = JSONServer.getInstance().addTask(task);
+		//Code returnCode = JSONServer.getInstance().addTask(task);
+		Code returnCode = Code.FAILURE;
+		try {
+			returnCode = new ModifyServerData(JSONServer.TaskType.AddTask,mContext).execute(task).get();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
 		if (returnCode == Code.SUCCESS) {
-			this.notifyAllObservers(JSONServer.getInstance().getAllTasks()); // updated
-																				// our
-																				// views
+			return sync(mContext); // updated
+																				// our														// views
+		}
+		return returnCode;
+	}
+	
+	public Code addSubmission(String taskId, Submission submission, Context mContext){
+		
+		Code returnCode = Code.FAILURE;
+		try {
+			returnCode = new AddSubmission(mContext).execute(new SubmissionData(taskId,submission)).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LOGGER.log(Level.INFO,"returnCode: " + returnCode.name());
+		if (returnCode == Code.SUCCESS) {
+			sync(mContext);
 		}
 		return returnCode;
 	}
 
-	public Code sync() {
+	public Code sync(Context mContext) {
 		// TODO add connection logic and locale file storage stuff logic
 		Code returnCode = JSONServer.getInstance().sync();
 		if (returnCode == Code.SUCCESS) {
-			this.notifyAllObservers(JSONServer.getInstance().getAllTasks()); // updated
+			
+			ArrayList<Task> tasks = new ArrayList<Task>();
+			try {
+				tasks = new ReceiveServerData(JSONServer.TaskType.GetTasks,mContext).execute().get();
+				TaskManager.cachedTasks = tasks;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			LOGGER.log(Level.INFO,"tasks null? " + (tasks==null));
+			
+			this.notifyAllObservers(tasks); // updated
 																				// our
 																				// views
 		}
