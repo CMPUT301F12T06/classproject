ebbdc6e1601b7c607df776138e465a3b02699f5c
Clinton <clint.isaac.wong@gmail.com>
2012-11-05 11:32:47 -0700

    Made new test cases

--- classproject/src/com/cmput301/classproject/Tests/LocalStorageTests.java
+++ classproject/src/com/cmput301/classproject/Tests/LocalStorageTests.java
@@ -2,9 +2,13 @@ package com.cmput301.classproject.Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.cmput301.classproject.Model.LocalStorage;
+import com.cmput301.classproject.Model.Submission;
+import com.cmput301.classproject.Model.Task;
 
 public class LocalStorageTests {
 
@@ -16,8 +20,20 @@ public class LocalStorageTests {
 	}
 	
 	@Test
-	public void testGetTasks(){
-		assertTrue(localStorage.load().size() > 0);
+	public void testNoEntries(){
+		assertTrue(localStorage.load().isEmpty());
 	}
 
+	@Test
+	public void testAddEntry(){
+		Task myTask = new Task("JUnit Local Task", "Description for Test Task", "Test Task Creator", Submission.ACCESS_PHOTO, true);
+		myTask.setId("JUnit Local Task Test");
+		List<Task> tasksList = localStorage.load();
+		int beforeCount = tasksList.size();
+		tasksList.add(myTask);
+		localStorage.save(tasksList);
+		int afterCount = localStorage.load().size();
+		assertTrue(afterCount > beforeCount);
+	}
+	
 }
